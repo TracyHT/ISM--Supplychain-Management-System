8d15081d81860c8e7a7bd592305bebd1bef21591
Tracy <131429059+TracyHT@users.noreply.github.com>
2025-05-03 13:15:19 +0700

    connect mongoDB

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ app.use("/products", productRoutes);
 /* MONGOOSE SETUP*/
 const PORT = process.env.PORT || 6001;
 mongoose
-  .connect(process.env.MONGO_URL, {
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
