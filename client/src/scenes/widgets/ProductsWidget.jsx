ea429d8acdecee0d20bf11eb6ccb71fa408494f1
Tracy <131429059+TracyHT@users.noreply.github.com>
2025-05-12 14:33:41 +0700

    feat(product): replace products listing from widgets to table

--- client/src/scenes/widgets/ProductsWidget.jsx
+++ client/src/scenes/widgets/ProductsWidget.jsx
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setProducts } from "../../state";
-import { Typography, Button, CircularProgress, Grid } from "@mui/material";
+import {
+  Typography,
+  Button,
+  CircularProgress,
+  Box,
+  useMediaQuery,
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+  IconButton,
+} from "@mui/material";
+import { Delete as DeleteIcon } from "@mui/icons-material";
 import Search from "../../components/Search";
 import Sort from "../../components/Sort";
 import Category from "../../components/Category";
-import { useMediaQuery } from "@mui/material";
-import { Box } from "@mui/material";
 import CustomPagination from "../../components/CustomPagination";
-import ProductWidget from "./ProductWidget";
+import { useNavigate } from "react-router-dom";
 
 const ProductsWidget = ({
   userId,
@@ -20,6 +33,7 @@ const ProductsWidget = ({
   const products = useSelector((state) => state.products);
   const token = useSelector((state) => state.token);
   const Role = useSelector((state) => state.user.role);
+  const navigate = useNavigate();
   const [sort, setSort] = useState(
     JSON.parse(localStorage.getItem("productSort")) || {
       sort: "quantity",
@@ -47,27 +61,49 @@ const ProductsWidget = ({
     localStorage.removeItem("productFilterName");
   };
 
-  // Cập nhật `useEffect` để kiểm tra `defaultStatus` và gọi API phù hợp
+  const columns = [
+    { label: "NAME", accessor: "name", show: true },
+    { label: "CATEGORY", accessor: "category", show: true },
+    { label: "PRICE", accessor: "price", show: true },
+    { label: "QUANTITY", accessor: "quantity", show: true },
+    {
+      label: "MIN QTY",
+      accessor: "minQuantity",
+      show: !(Role === "employee" && defaultStatus === "Inventory"),
+    },
+    { label: "MAX QTY", accessor: "maxQuantity", show: true },
+    {
+      label: "REORDER POINT",
+      accessor: "reorderPoint",
+      show: !(Role === "employee" && defaultStatus === "Marketplace"),
+    },
+    {
+      label: "ACTION",
+      accessor: "actions",
+      show:
+        (Role === "employee" && defaultStatus === "Inventory") ||
+        (Role === "supplier" && defaultStatus === "Marketplace"),
+    },
+  ];
+
+  const visibleColumns = columns.filter((column) => column.show);
+
   useEffect(() => {
     const getProducts = async () => {
       try {
         setLoading(true);
         const filterCategoryString = filterCategory.join(",");
-
         const baseUrl =
           defaultStatus === "Marketplace"
             ? "http://localhost:6001/products"
-            : `http://localhost:6001/inventory/${userId}/inventory`; // Gọi API cho Inventory
+            : `http://localhost:6001/inventory/${userId}/inventory`;
 
         const queryParams = `?page=${page}&sort=${sort.sort},${sort.order}&category=${filterCategoryString}&search=${search}&name=${filterName}`;
-
         const response = await fetch(`${baseUrl}${queryParams}`, {
           method: "GET",
           headers: { Authorization: `Bearer ${token}` },
         });
-
         const data = await response.json();
-        console.log("Inventory data:", data);
         dispatch(setProducts({ products: data }));
         setLoading(false);
       } catch (error) {
@@ -80,18 +116,13 @@ const ProductsWidget = ({
       try {
         setLoading(true);
         const filterCategoryString = filterCategory.join(",");
-
         const baseUrl = `http://localhost:6001/products/${userId}/products`;
-
         const queryParams = `?page=${page}&sort=${sort.sort},${sort.order}&category=${filterCategoryString}&search=${search}&name=${filterName}`;
-
         const response = await fetch(`${baseUrl}${queryParams}`, {
           method: "GET",
           headers: { Authorization: `Bearer ${token}` },
         });
-
         const data = await response.json();
-        console.log("Inventory data:", data);
         dispatch(setProducts({ products: data }));
         setLoading(false);
       } catch (error) {
@@ -101,27 +132,31 @@ const ProductsWidget = ({
     };
 
     const getBookedProducts = async () => {
-      setLoading(true);
-      const filterCategoryString = filterCategory.join(",");
-
-      const response = await fetch(
-        `http://localhost:6001/products/${userId}/bookedproducts?page=${page}&sort=${sort.sort},${sort.order}&category=${filterCategoryString}&search=${search}&name=${filterName}`,
-        {
-          method: "GET",
-          headers: { Authorization: `Bearer ${token}` },
-        }
-      );
-      const data = await response.json();
-      dispatch(setProducts({ products: data }));
-      setLoading(false);
+      try {
+        setLoading(true);
+        const filterCategoryString = filterCategory.join(",");
+        const response = await fetch(
+          `http://localhost:6001/products/${userId}/bookedproducts?page=${page}&sort=${sort.sort},${sort.order}&category=${filterCategoryString}&search=${search}&name=${filterName}`,
+          {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` },
+          }
+        );
+        const data = await response.json();
+        dispatch(setProducts({ products: data }));
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching booked products:", error);
+        setLoading(false);
+      }
     };
 
-    // Điều kiện gọi API tùy thuộc vào `defaultStatus`
     if (Role === "supplier") {
       getSupplierProduct();
-    } else if (defaultStatus === "Marketplace") {
-      getProducts();
-    } else if (defaultStatus === "Inventory") {
+    } else if (
+      defaultStatus === "Marketplace" ||
+      defaultStatus === "Inventory"
+    ) {
       getProducts();
     } else if (isBookedProducts) {
       getBookedProducts();
@@ -135,7 +170,7 @@ const ProductsWidget = ({
     userId,
     isProfile,
     token,
-    defaultStatus, // Re-run effect nếu `defaultStatus` thay đổi
+    defaultStatus,
   ]);
 
   const handleSortChange = (newSort) => {
@@ -164,6 +199,29 @@ const ProductsWidget = ({
     localStorage.setItem("productPage", newPage);
   };
 
+  const handleViewDetails = (productId) =>
+    navigate(`/products/${productId}/product`);
+
+  const handleDeleteProduct = async () => {
+    try {
+      const response = await fetch(
+        `http://localhost:6001/products/${productUserId}/${productId}/delete`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const result = await response.json();
+      dispatch(setProduct({ product: result }));
+      navigate("/delete");
+    } catch (error) {
+      console.error("Failed to delete product:", error);
+    }
+  };
+
   return (
     <>
       {loading ? (
@@ -179,104 +237,111 @@ const ProductsWidget = ({
         <Box>
           {/* Filters */}
           <Box mt={3}>
-            <Box display={"flex"} gap={isNonMobile ? 7 : 2}>
-              <Box>
-                <Sort sort={sort} setSort={handleSortChange} />
-              </Box>
+            <Box display="flex" gap={isNonMobile ? 7 : 2}>
+              <Sort sort={sort} setSort={handleSortChange} />
             </Box>
-            <Box mt={2}>
-              {/* First row of search inputs */}
-              <Box display="flex" gap={2} mb={2}>
-                <Search
-                  value={filterName}
-                  onChange={handleFilterNameChange}
-                  placeholder="Search by Product Name"
-                />
-              </Box>
+
+            <Box mt={2} display="flex" gap={2}>
+              <Search
+                value={filterName}
+                onChange={handleFilterNameChange}
+                placeholder="Search by Product Name"
+              />
             </Box>
 
             <Box mt={2}>
               <Category
                 filterCategory={filterCategory}
-                category={products.category ? products.category : []}
+                category={products.category || []}
                 setFilterCategory={handleFilterCategoryChange}
               />
             </Box>
 
             <Box mt={2}>
-              {Role === "employee" ? (
-                <Button variant="outlined" onClick={clearFilters}>
-                  <Typography fontSize={"1rem"}> Clear Filters</Typography>
-                </Button>
-              ) : (
-                <Button
-                  variant="outlined"
-                  sx={{
-                    fontSize: "1.25rem",
-                    padding: "12px 24px",
-                    color: "#834bff",
-                    borderColor: "#834bff",
-                    "&:hover": {
-                      color: "#fff",
-                      backgroundColor: "#834bff",
-                      borderColor: "#834bff",
-                    },
-                  }}
-                  onClick={clearFilters}
-                >
-                  <Typography fontSize={"1rem"}> Clear Filters</Typography>
-                </Button>
-              )}
+              <Button
+                variant="outlined"
+                onClick={clearFilters}
+                sx={
+                  Role === "employee"
+                    ? {}
+                    : {
+                        fontSize: "1.25rem",
+                        padding: "12px 24px",
+                        color: "#834bff",
+                        borderColor: "#834bff",
+                        "&:hover": {
+                          color: "#fff",
+                          backgroundColor: "#834bff",
+                          borderColor: "#834bff",
+                        },
+                      }
+                }
+              >
+                <Typography fontSize="1rem">Clear Filters</Typography>
+              </Button>
             </Box>
-            {!isBookedProducts}
           </Box>
-          {/* Product Listings */}
-          <Box>
-            <Grid container spacing={3} mt={2}>
-              {Array.isArray(products.products) &&
-                products.products.map(
-                  ({
-                    _id,
-                    userId,
-                    name,
-                    description,
-                    price,
-                    quantity,
-                    minQuantity,
-                    reorderPoint,
-                    maxQuantity,
-                    category,
-                    bookings,
-                    orders,
-                  }) => (
-                    <Grid item xs={12} sm={6} key={_id}>
-                      <ProductWidget
-                        productId={_id}
-                        productUserId={userId}
-                        name={name}
-                        description={description}
-                        price={price}
-                        quantity={quantity}
-                        minQuantity={minQuantity}
-                        reorderPoint={reorderPoint}
-                        maxQuantity={maxQuantity}
-                        category={category}
-                        bookings={bookings}
-                        defaultStatus={defaultStatus}
-                        orders={orders}
-                      />
-                    </Grid>
-                  )
-                )}
-            </Grid>
-            <Box display={"flex"} justifyContent={"center"} mt={4}>
-              <CustomPagination
-                page={page}
-                limit={products.limit ? products.limit : 0}
-                total={products.total ? products.total : 0}
-                setPage={handlePageChange}
-              />
-            </Box>
+
+          {/* Table Display */}
+          <TableContainer component={Paper} sx={{ mt: 3 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {visibleColumns.map((column) => (
+                    <TableCell
+                      key={column.accessor}
+                      sx={{ color: "text.secondary" }}
+                    >
+                      {column.label}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {products.products?.map((product) => (
+                  <TableRow key={product._id}>
+                    {visibleColumns.map((column) => (
+                      <TableCell key={column.accessor}>
+                        {column.accessor === "name" ? (
+                          <Typography
+                            sx={{
+                              cursor: "pointer",
+                              "&:hover": { textDecoration: "underline" },
+                            }}
+                            onClick={() => handleViewDetails(product._id)}
+                          >
+                            {product.name}
+                          </Typography>
+                        ) : column.accessor === "actions" ? (
+                          <IconButton
+                            onClick={() =>
+                              handleDeleteProduct(product._id, product.userId)
+                            }
+                            // color="error"
+                          >
+                            <DeleteIcon />
+                          </IconButton>
+                        ) : column.accessor === "price" ? (
+                          `$${product[column.accessor] || "N/A"}`
+                        ) : (
+                          product[column.accessor] || "N/A"
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          {/* Pagination */}
+          <Box display="flex" justifyContent="center" mt={4}>
+            <CustomPagination
+              page={page}
+              limit={products.limit || 0}
+              total={products.total || 0}
+              setPage={handlePageChange}
+            />
           </Box>
         </Box>
       )}
