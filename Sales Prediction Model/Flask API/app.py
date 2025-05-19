9394ec2aad5534950d895ba798e1bd1e67204263
Nguyen Duc Toan <125717928+duwcston@users.noreply.github.com>
2025-05-19 15:26:36 +0700

    feat(predictions): add daily prediction functionality and update UI for predictionPage

--- Sales Prediction Model/Flask API/app.py
+++ Sales Prediction Model/Flask API/app.py
@@ -31,6 +31,32 @@ def monthlyPrediction():
     }
     
     return jsonify(json_output)
+
+@app.route('/predictDaily', methods=['GET', 'POST'])
+def dailyPrediction():
+    if "day" not in request.args or "month" not in request.args or "year" not in request.args:
+        return jsonify({"error": "Invalid arguments passed. Must include 'day', 'month', and 'year'."})
+        
+    day = int(request.args["day"])
+    month = int(request.args["month"])
+    year = int(request.args["year"])
+    
+    X_test = pd.DataFrame(np.array([year, month, day]).reshape(1, -1), columns=["Year", "Month", "Day"])
+    
+    # Load the model
+    loaded_model = joblib.load("../models/randomforest-daily.pkl")
+    
+    # Make predictions
+    y_pred = loaded_model.predict(X_test)
+    
+    json_output = {
+        "P1": int(y_pred[0][0]),
+        "P2": int(y_pred[0][1]),
+        "P3": int(y_pred[0][2]),
+        "P4": int(y_pred[0][3]),
+    }
+    
+    return jsonify(json_output)
     
     
 @app.route('/')
